91a7145d7d62572693153994d2aa398246d49bcf
devdyaya <sho.niida@shelfy.jp>
2020-06-18 21:19:50 +0900

    for回してDBにつっこみたいんですけど

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # togatter_test
+
+MySQLのテーブル設計
+
+- データベース名: Dyy_togatter
+- テーブル名: comment_list
+- ID名: id
+- カラム名: comment
--- script.py
+++ script.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import mysql.connector # MySQL
 
+url = 'https://b.hatena.ne.jp/togetter/bookmark?page={0}'
+
 # DB接続
 def data_registration_single(self):
     connection = mysql.connector.connect(
@@ -13,27 +15,20 @@ def data_registration_single(self):
     )
     cursor = connection.cursor()
     cursor.execute("USE comment_list")
-    cursor.execute("select * from user;")
+    connection.commit()
 
-    for row in cursor.fetchall():
-        print(row[0])
+    for i in range(1,3):
+        r = requests.get(url.format(i))
+        soup = BeautifulSoup(r.text,'html.parser')
+        results = soup.find_all(class_='js-comment')
+        for result in results:
+            i = i + 1
+            print(result.get_text())
+
+            # データ挿入
+            cursor.execute("INSERT INTO comment_list(comment) VALUES (%s)", (self['comment']))
+            connection.commit()
+            printf('---DB登録に成功---')
 
     cursor.close
     connection.close
-
-    # connection.commit()
-    #データ挿入
-    # cursor.execute("INSERT INTO comment_list(comment) VALUES (%s)", (self['comment_list']))
-    # connection.commit()
-    # printf('---DB登録に成功---')
-
-
-url = 'https://b.hatena.ne.jp/togetter/bookmark?page={0}'
-
-for i in range(1,3):
-    r = requests.get(url.format(i))
-    soup = BeautifulSoup(r.text,'html.parser')
-    results = soup.find_all(class_='js-comment')
-    for result in results:
-        i = i + 1
-        print(result.get_text())
